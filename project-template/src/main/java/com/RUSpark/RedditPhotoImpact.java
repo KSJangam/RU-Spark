3b07f44104a8c5fde84ef712cbe34cfde4fe4261
Kunal Jangam <kjangam8@gmail.com>
2021-04-01 13:59:18 -0400

    debugging photoimpact

--- project-template/src/main/java/com/RUSpark/RedditPhotoImpact.java
+++ project-template/src/main/java/com/RUSpark/RedditPhotoImpact.java
@@ -16,7 +16,7 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.SparkSession;
 
 public class RedditPhotoImpact {
-	 private static final Pattern SPACE = Pattern.compile(" ");
+	// private static final Pattern SPACE = Pattern.compile(" ");
 	public static void main(String[] args) throws Exception {
 
     if (args.length < 1) {
@@ -27,18 +27,22 @@ public class RedditPhotoImpact {
 		String InputPath = args[0];
 		  SparkSession spark = SparkSession
 			      .builder()
-			      .appName("WordCount")
+			      .appName("RedditPhotoImpact")
 			      .getOrCreate();
-
+	System.out.println(1);
 	JavaRDD<Row> rows = spark.read().csv(InputPath).javaRDD();
+	System.out.println(2);
 	//JavaRDD<Tuple2<String, Integer>> impacts = rows.map(s -> new Tuple2<String, Integer>((String)s.get(0), (Integer.parseInt((String) s.get(4))+Integer.parseInt((String) s.get(5)) +Integer.parseInt((String) s.get(6)))));
 	JavaPairRDD<String, Integer> impacts = rows.mapToPair(s -> new Tuple2<>((String)s.getString(0), Integer.parseInt((String) s.get(4))+Integer.parseInt((String) s.get(5)) +Integer.parseInt((String) s.get(6))));
+	System.out.println(3);
 	 JavaPairRDD<String, Integer> summedImpacts = impacts.reduceByKey((i1, i2) -> i1 + i2);
+	 System.out.println(4);
 	 List<Tuple2<String, Integer>> output = summedImpacts.collect();
+	 System.out.println(5);
 	    for (Tuple2<?,?> tuple : output) {
 	      System.out.println(tuple._1() + " " + tuple._2());
 	    }
-			
+	    System.out.println(6);
 	    spark.stop();
 		/* Implement Here */ 
 		
