97b62e8b74d8b12feb12503fc766ee838f54439d
Kunal Jangam <kjangam8@gmail.com>
2021-04-02 13:27:15 -0400

    intial hourimpact

--- project-template/src/main/java/com/RUSpark/RedditHourImpact.java
+++ project-template/src/main/java/com/RUSpark/RedditHourImpact.java
@@ -2,8 +2,12 @@ package com.RUSpark;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import scala.Tuple2;
+
+import java.sql.Date;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -11,7 +15,7 @@ import java.util.regex.Pattern;
 /* any necessary Java packages here */
 
 public class RedditHourImpact {
-	 private static final Pattern SPACE = Pattern.compile(" ");
+	// private static final Pattern SPACE = Pattern.compile(" ");
 	public static void main(String[] args) throws Exception {
 
     if (args.length < 1) {
@@ -20,7 +24,26 @@ public class RedditHourImpact {
     }
 		
 		String InputPath = args[0];
-		
+		  SparkSession spark = SparkSession
+			      .builder()
+			      .appName("RedditHourImpact")
+			      .getOrCreate();
+	
+	JavaRDD<Row> rows = spark.read().csv(InputPath).javaRDD();
+	
+	//JavaRDD<Tuple2<String, Integer>> impacts = rows.map(s -> new Tuple2<String, Integer>((String)s.get(0), (Integer.parseInt((String) s.get(4))+Integer.parseInt((String) s.get(5)) +Integer.parseInt((String) s.get(6)))));
+	JavaPairRDD<Integer, Integer> impacts = rows.mapToPair(s -> new Tuple2<>(new Date(s.getLong(1)*1000).getHours(), (s.getInt(4)+s.getInt(5)+s.getInt(6))));
+	
+	 JavaPairRDD<Integer, Integer> summedImpacts = impacts.reduceByKey((i1, i2) -> i1 + i2);
+	
+	 List<Tuple2<Integer, Integer>> output = summedImpacts.collect();
+	 	for(int i=0; i<23; i++) {
+	    for (Tuple2<?,?> tuple : output) {
+	    	if(tuple._1().equals(i))
+	      System.out.println(tuple._1() + " " + tuple._2());
+	    }
+	 	}
+	    spark.stop();
 		/* Implement Here */ 
 		
 	}
