aa076f3132134aa72c738c14f183c603d43e7aa3
Kunal Jangam <kjangam8@gmail.com>
2021-04-02 13:45:06 -0400

    debugging 2

--- project-template/src/main/java/com/RUSpark/RedditPhotoImpact.java
+++ project-template/src/main/java/com/RUSpark/RedditPhotoImpact.java
@@ -34,14 +34,14 @@ public class RedditPhotoImpact {
 
 	  System.out.println(2);
 	//JavaRDD<Tuple2<String, Integer>> impacts = rows.map(s -> new Tuple2<String, Integer>((String)s.get(0), (Integer.parseInt((String) s.get(4))+Integer.parseInt((String) s.get(5)) +Integer.parseInt((String) s.get(6)))));
-	JavaPairRDD<String, Integer> impacts = rows.mapToPair(s -> new Tuple2<>(s.getString(0), (s.getInt(4))+s.getInt(5)+s.getInt(6)));
+	JavaPairRDD<String, Integer> impacts = rows.mapToPair(s -> new Tuple2<>(s.getString(0), (s.getInt(4)+s.getInt(5)+s.getInt(6))));
 
 	  System.out.println(3);
 	 JavaPairRDD<String, Integer> summedImpacts = impacts.reduceByKey((i1, i2) -> i1 + i2);
 
 	  System.out.println(4);
 	 List<Tuple2<String, Integer>> output = summedImpacts.collect();
-	
+	System.out.println(5);
 	    for (Tuple2<?,?> tuple : output) {
 	      System.out.println(tuple._1() + " " + tuple._2());
 	    }
