06f7c808c747744223c1c6f14d3210854fedf49f
Kunal Jangam <kjangam8@gmail.com>
2021-04-02 14:25:31 -0400

    fixed an error

--- project-template/src/main/java/com/RUSpark/RedditHourImpact.java
+++ project-template/src/main/java/com/RUSpark/RedditHourImpact.java
@@ -31,7 +31,7 @@ public class RedditHourImpact {
 		  System.out.println(2);
 	JavaRDD<Row> rows = spark.read().csv(InputPath).javaRDD();
 	System.out.println(3);
-	JavaPairRDD<Integer, Integer> impacts = rows.mapToPair(s -> new Tuple2<>(new Date(Long.parseLong((String)s.get(0))*1000).getHours(), (Integer.parseInt((String) s.get(4))+Integer.parseInt((String) s.get(5)) +Integer.parseInt((String) s.get(6)))));
+	JavaPairRDD<Integer, Integer> impacts = rows.mapToPair(s -> new Tuple2<>(new Date(Long.parseLong((String)s.get(1))*1000).getHours(), (Integer.parseInt((String) s.get(4))+Integer.parseInt((String) s.get(5)) +Integer.parseInt((String) s.get(6)))));
 	//JavaPairRDD<Integer, Integer> impacts = rows.mapToPair(s -> new Tuple2<>(new Date(s.getLong(1)*1000).getHours(), (s.getInt(4)+s.getInt(5)+s.getInt(6))));
 	System.out.println(4);
 	 JavaPairRDD<Integer, Integer> summedImpacts = impacts.reduceByKey((i1, i2) -> i1 + i2);
