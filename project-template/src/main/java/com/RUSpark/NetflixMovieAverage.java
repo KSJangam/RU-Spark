7bc0058a5ca08dd9488fecfdcac7fa5636553dd4
Kunal Jangam <kjangam8@gmail.com>
2021-04-04 15:47:13 -0400

    debugging movieavg5

--- project-template/src/main/java/com/RUSpark/NetflixMovieAverage.java
+++ project-template/src/main/java/com/RUSpark/NetflixMovieAverage.java
@@ -48,9 +48,9 @@ public class NetflixMovieAverage {
 	    for (Tuple2<?,?> tuple : ratingsOutput) {
 	    	for(Tuple2<?,?> tuple2 : countsOutput) {
 	    		if(((String)tuple._1()).equals((String)tuple2._1())) {
-	    			System.out.println("hello");
-	    		//  System.out.println((String)tuple._1() + " " + String.format("%.2f", (Double.parseDouble((String)tuple._2()))/(Double.parseDouble((String)tuple2._2()))));
-	    			 System.out.println((String)tuple._1() + " " + Double.parseDouble((String)tuple._2())/(Double.parseDouble((String)tuple2._2())));
+	    			System.out.println("helloo");
+	    		 System.out.println((String)tuple._1() + " " + String.format("%.2f", (Integer.parseInt((String)tuple._2()))*1.0/(Integer.parseInt((String)tuple2._2()))));
+	    		//	 System.out.println((String)tuple._1() + " " + Double.parseDouble((String)tuple._2())/(Double.parseDouble((String)tuple2._2())));
 	 	    		System.out.println("hi");
 	    			break;
 	    		}
