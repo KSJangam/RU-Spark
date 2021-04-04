f0239149e758b5639054ce6341ac1ed972555f17
Kunal Jangam <kjangam8@gmail.com>
2021-04-04 14:29:23 -0400

    initial Netflix code

--- project-template/src/main/java/com/RUSpark/NetflixGraphGenerate.java
+++ project-template/src/main/java/com/RUSpark/NetflixGraphGenerate.java
@@ -2,8 +2,12 @@ package com.RUSpark;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import scala.Tuple2;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -11,18 +15,54 @@ import java.util.regex.Pattern;
 /* any necessary Java packages here */
 
 public class NetflixGraphGenerate {
-	 private static final Pattern SPACE = Pattern.compile(" ");
+	
+	public static List<String> generateStringList(String s) {
+		List<String> a = new ArrayList<String>();
+		String[] s2 = s.split(" ");
+		for(String str: s2) {
+			a.add(str);
+		}
+		return a;
+	}
+	private static List<String> generatePairs(List<String> s) {
+		List<String> a = new ArrayList<String>();
+		for(String str:s) {
+			for(String str2:s) {
+				if(Integer.parseInt(str)<Integer.parseInt(str2)) {
+					a.add("("+str+", "+str2+")");
+				}
+			}
+		}
+		return a;
+	}
+	//private static final Pattern SPACE = Pattern.compile(" ");
 	public static void main(String[] args) throws Exception {
 
     if (args.length < 1) {
       System.err.println("Usage: NetflixGraphGenerate <file>");
       System.exit(1);
     }
-		
+    SparkSession spark = SparkSession
+		      .builder()
+		      .appName("NetflixGraphGenerate")
+		      .getOrCreate();
 		String InputPath = args[0];
-		
+		JavaRDD<Row> rows = spark.read().csv(InputPath).javaRDD();
+		JavaPairRDD<Tuple2<String, Integer>, String> mrpair = rows.mapToPair(s->new Tuple2<>(new Tuple2<>((String)s.getString(0), Integer.parseInt((String)s.get(2))), (String)s.getString(1)));
+		JavaPairRDD<Tuple2<String, Integer>, String>  summedMRPairs = mrpair.reduceByKey((i1, i2) -> i1 +" "+ i2);
+		JavaPairRDD<Tuple2<String, Integer>, List<String>>  summedMRPairs2 = summedMRPairs.mapToPair(s->(new Tuple2<>(s._1(), generateStringList(s._2()))));
+		JavaRDD<List<String>> connectedIds = summedMRPairs2.map(s->s._2());
+		JavaRDD<List<String>> connectedIds2=connectedIds.map(s->generatePairs(s));
+		JavaRDD<String> nodes = connectedIds2.flatMap(s->(s.iterator()));
+		JavaPairRDD<String, Integer> edges = nodes.mapToPair(s->new Tuple2<>(s, 1));
+		JavaPairRDD<String, Integer> summedEdges = edges.reduceByKey((i1, i2) -> i1 + i2);
+		List<Tuple2<String, Integer>> output = summedEdges.collect();
+		for (Tuple2<?,?> tuple : output) {
+		      System.out.println(tuple._1() + " " + tuple._2());
+		    }
 		/* Implement Here */ 
 		
 	}
+	
 
 }
--- project-template/src/main/java/com/RUSpark/NetflixMovieAverage.java
+++ project-template/src/main/java/com/RUSpark/NetflixMovieAverage.java
@@ -2,7 +2,10 @@ package com.RUSpark;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +14,7 @@ import java.util.regex.Pattern;
 /* any necessary Java packages here */
 
 public class NetflixMovieAverage {
-	 private static final Pattern SPACE = Pattern.compile(" ");
+	// private static final Pattern SPACE = Pattern.compile(" ");
 	public static void main(String[] args) throws Exception {
 
     if (args.length < 1) {
@@ -20,7 +23,39 @@ public class NetflixMovieAverage {
     }
 		
 		String InputPath = args[0];
-		
+		  SparkSession spark = SparkSession
+			      .builder()
+			      .appName("NetflixMovieAverage")
+			      .getOrCreate();
+		  System.out.println(1);
+	JavaRDD<Row> rows = spark.read().csv(InputPath).javaRDD();
+	JavaPairRDD<String, Integer> ones = rows.mapToPair(s -> new Tuple2<>((String)s.getString(0), 1));
+
+    JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+    List<Tuple2<String, Integer>> countsOutput = counts.collect();
+	  System.out.println(2);
+	//JavaRDD<Tuple2<String, Integer>> impacts = rows.map(s -> new Tuple2<String, Integer>((String)s.get(0), (Integer.parseInt((String) s.get(4))+Integer.parseInt((String) s.get(5)) +Integer.parseInt((String) s.get(6)))));
+	//JavaPairRDD<String, Integer> impacts = rows.mapToPair(s -> new Tuple2<>(s.getString(0), (s.getInt(4)+s.getInt(5)+s.getInt(6))));
+	  JavaPairRDD<String, Integer> ratings = rows.mapToPair(s -> new Tuple2<>((String)s.getString(0), Integer.parseInt((String) s.get(2))));
+
+	  System.out.println(3);
+	 JavaPairRDD<String, Integer> summedRatings = ratings.reduceByKey((i1, i2) -> i1 + i2);
+
+	  System.out.println(4);
+	
+	 List<Tuple2<String, Integer>> ratingsOutput = summedRatings.collect();
+	System.out.println(5);
+	    for (Tuple2<?,?> tuple : ratingsOutput) {
+	    	for(Tuple2<?,?> tuple2 : countsOutput) {
+	    		if(((String)tuple._1()).equals((String)tuple._2())) {
+	    		  System.out.println(tuple._1() + " " + String.format("%.2f", (Double.parseDouble((String) tuple._2())/Double.parseDouble((String) tuple2._2()))));
+	    		  break;
+	    		}
+	    			 
+	    	}
+	       }
+	   
+	    spark.stop();
 		/* Implement Here */ 
 		
 	}
